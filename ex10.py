6cd3ea27bb2e277b64a3ed5ddcc6febc457d76c1
muhamed <thenveer81@gmail.com>
2019-12-12 10:24:44 +0530

    initial commit

---
+++ ex10.py
@@ -0,0 +1,17 @@
+tabby_cat ="\t'm tabbled in."
+persian_cat="i'am slit\non a line."
+backlash_cat="i'am \\ a\\ cat."
+
+fat_cay="""
+i'll do a list:
+\t* cat food
+\t* finshed
+\t* catnip\n\t*grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backlash_cat)
+
+print(fat_cay)
+
