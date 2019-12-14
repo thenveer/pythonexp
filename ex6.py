4e9f4f0e1ecc2312c9371b51d610c3747c08e546
muhamed <thenveer81@gmail.com>
2019-12-14 19:31:48 +0530

    second commit

---
+++ ex6.py
@@ -0,0 +1,21 @@
+ty_of_pe = 10
+x=f"there are {ty_of_pe} type of people."
+
+binary = "binary"
+do_not = "don't"
+
+y=f"those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+print (f"i said:{x}")
+print (f"i also said:'{y}'")
+
+hilarious = False
+jo_eva = "yes i got it{}"
+print(jo_eva.format(hilarious))
+w = "hey"
+j = "to you"
+
+print (w+j)
+
---
+++ pp.py
@@ -0,0 +1,16 @@
+class point:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def make(self):
+        if(self.a >0 and self.b>0):
+            return "this is 1st"
+        elif (self.a<0 and self.b>0):
+            return "this is 2nd"
+        elif (self.a<0 and self.b<0):
+            return "this is 3rd"
+        else:
+            return"this is 4rd"
+p=point(-1,-2)
+print(p.make())
