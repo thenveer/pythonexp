445b2ea70b2bd584d63c6e65c14e19373759c490
muhamed <thenveer81@gmail.com>
2019-12-23 15:08:48 +0530

    last commit on 23

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("hoe are old are you?\n", end='')
+age =input()
+print("how tall are you?",end='')
+hight=input()
+print("how much do you weight?", end='')
+wight= input()
+
+print(f"so, you r {age} old {hight} tall and {wight} heavy")
